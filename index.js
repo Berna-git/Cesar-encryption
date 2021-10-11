eb0ce3f3c62b00c8bfedc60ee1906670012b7b4f
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-10-11 17:54:32 -0300

    Encriptado funcionando, falta funcionalidad desencriptar

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 //Traigo datos!
 const butonConversor = document.querySelector('#btn-conver');
+const devolution_msj = document.querySelector('#section-devolution_msj')
 //Agrego evento al boton
 butonConversor.addEventListener("click", converText);
-// butonConversor.addEventListener("click", getDataValue);
-// butonConversor.addEventListener("click", getDataText);
 
-//array Prueba 
+//abecedario
 const abecedario = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
-const abcDesorder = ["z"," ","b","e","r","n","a"];
+
 //Traigo Los datos del textarea y el Root
 const getDataText = () => {
     const getText = document.querySelector('#section-textArea_value');
     const valueText = getText.value;
-     return valueText;
+    return valueText;
 }
 const getDataValue = () => {
     const getRoot = document.querySelector('#selcionardor-root');
@@ -27,42 +26,42 @@ const converToArray = (texto) => {
 }
 
 //Encriptar el mensaje
-const encryptMessage = (textoConverso) => {
-    // let result = []
-    // for (item in abcDesorder ){
-    //     result.push(abecedario[item ])
-    //     console.log(result);
-    // }
+const encryptMessage = (textoToConverso,ValueRot) => {
     let result= [];
-    for (let i = 0; abcDesorder.length > i; i++ ){
+    for (let i = 0; textoToConverso.length > i; i++ ){
         for(let j = 0; abecedario.length > j; j++){
-            if(abcDesorder[i] === abecedario[j]){
-                result.push(abecedario[j + 2])
+            if(textoToConverso[i] === abecedario[j]){
+                result.push(abecedario[j + ValueRot])
                 break;
-            } else if(abcDesorder[i] == " ") {
-                result.push(abcDesorder[i]);
+            } else if(textoToConverso[i] == " ") {
+                result.push(textoToConverso[i]);
                 break;
             }
         }
     } 
-    console.log(result);
+    // console.log(result);
+    return result;
 
 }
+// const printMsj = () => {
+
+// }
 
 // Funcion para convertir
 function converText() {
-    // dentro de esta funcion habra funciones
-    //covierta el string en un array
-    //pasar el el array y decodificar el msj
-    // pasarlo a string
-    const takeDataValue = getDataValue();
+    // Traigo datos
+    const takeDataValue = parseInt(getDataValue());
     const takeDataText = getDataText();
-    // console.log(takeDataValue)
-    // console.log(takeDataText)
+    // Convierto en array
     const arrayText = converToArray(takeDataText);
-    encryptMessage();    
+    // Encripto msj y devuelvo array
+    const encryptMsjArray = encryptMessage(arrayText,takeDataValue);
+    // Convierto un array en string 
+    let msjEcrypted = encryptMsjArray.join("")
+    devolution_msj.innerHTML = msjEcrypted;    
+    
 }
-encryptMessage();    
+// encryptMessage();    
 
 
 
