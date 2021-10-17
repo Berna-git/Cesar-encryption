610d407bc699ab87c5d82ef8dda894d28a5d0ff4
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-10-16 22:37:00 -0300

    encriptado y desifrado andando faltan estilos

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ butonConversor.addEventListener("click", converText);
 btnCifrarMsj.addEventListener("click", activeCifrado);
 btnDesifrarMsj.addEventListener("click", activeDesifrado); 
 
-
 //abecedario
-const abecedario = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
+const abecedario = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","u","v","w","x","y","z"];
 const otherSimbols = ["1","2","3","4","5","6","7","8","9","0",",","."," "];
 //Traigo Los datos del textarea y el Root
 const getDataText = () => {
@@ -30,39 +29,42 @@ const converToArray = (texto) => {
    let arrayText = Array.from(texto.toLowerCase());
     return arrayText; 
 }
-//
-//Encriptar el mensaje
-const encryptMessage = (textoToConverso,ValueRot) => {
+
+//Encriptar y desencriptar el mensaje
+const encryptMessage = (textoToConverso,valueRot) => {
     let result= [];
     for (let i = 0; textoToConverso.length > i; i++ ){
         for(let j = 0; abecedario.length > j; j++){
-            if(otherSimbols.includes(textoToConverso[i])) {
-                result.push(textoToConverso[i])
-                console.log("no estoy tan al pedo");
-                break;
-            }else if(textoToConverso[i] === abecedario[j]){
-                result.push(abecedario[j + ValueRot])
-                break;
+                if(otherSimbols.includes(textoToConverso[i])) {
+                    result.push(textoToConverso[i])
+                    console.log("no estoy tan al pedo");
+                    break;
+                } else if(cifrado && textoToConverso[i] === abecedario[j]) {    
+                     result.push(abecedario[j + valueRot])
+                     break;
+                }else if(textoToConverso[i] === abecedario[j]){
+                    result.push(abecedario[(j + 26) - valueRot])
+                    break;
+                }
             }
         }
-    } 
+        return result;
+    }
+
     // console.log(result);
-    return result;
-
-}
-// const printMsj = () => {
-
-// }
-//Activar y Desactivar el cifradi
-
+    
+//Activar y Desactivar el cifrado
+let cifrado = true
 function activeDesifrado() {
     cifrado = false;
-    console.log("boton desifrado");
+    btnDesifrarMsj.classList.add("boton-click");
+    btnCifrarMsj.classList.remove("boton-click");
     return cifrado;    
 }
 function activeCifrado () {
     cifrado = true;
-    console.log("boton cifrado");
+    btnCifrarMsj.classList.add("boton-click");
+    btnDesifrarMsj.classList.remove("boton-click");
     return cifrado;
 }
 
@@ -78,10 +80,9 @@ function converText() {
     const encryptMsjArray = encryptMessage(arrayText,takeDataValue);
     // Convierto un array en string 
     let msjEcrypted = encryptMsjArray.join("")
-    devolution_msj.innerHTML = msjEcrypted;    
+    devolution_msj.innerHTML = msjEcrypted;      
     console.log(cifrado)
 }
-// encryptMessage();    
 
 
 
