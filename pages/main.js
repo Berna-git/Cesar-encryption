ab8dead449d611fa78a4d3e3c1ba8f1187900913
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-11-18 22:19:02 -0300

    Finish Work

--- pages/main.js
+++ pages/main.js
@@ -1,10 +1,9 @@
 // get Elements 
 const btnComparation = document.getElementById('worlds-comparacion');
-
+const divMSjResult = document.getElementById('msj-result');
 // Add Listeners 
 btnComparation.addEventListener('click', comparation)
-
-// funciotiones of convert array and sort 
+// function of convert array and sort 
 function convertArrayAndOrder(strToConvert) {
     strToConvert = strToConvert.toLowerCase();
     const convertWorld_arr = strToConvert.split('');
@@ -13,15 +12,23 @@ function convertArrayAndOrder(strToConvert) {
     return world_OrderStr;
 }
 
-
+// Comparation and return the msj for the user! 
 function comparation() {
     const comparationWorld_one = document.getElementById('entrada-world-one').value;
     const comparationWorld_two = document.getElementById('entrada-world-two').value;
     const world_one_str_order = convertArrayAndOrder(comparationWorld_one);
     const world_two_str_order = convertArrayAndOrder(comparationWorld_two);
-    if ( world_one_str_order == world_two_str_order) {
-        console.log("Es un anagrama")
+    divMSjResult.classList.remove("style-MsjRed")
+    divMSjResult.classList.remove("style-Msjgreen")
+    if ( world_one_str_order == world_two_str_order && world_one_str_order && world_two_str_order) {
+        divMSjResult.classList.add("style-Msjgreen");
+        divMSjResult.innerHTML = `<p>"${comparationWorld_two}" es Anagrama de "${comparationWorld_one}"</p>`        
+    } else if (!world_one_str_order && !world_two_str_order) {
+        divMSjResult.classList.add("style-MsjRed");
+        divMSjResult.innerHTML = `<p>Alguna de las entradas estan vacias o las dos!</p>`        
     } else {
-        console.log('Anagrama no es')
+        divMSjResult.classList.add("style-MsjRed");
+        divMSjResult.innerHTML = `<p>"${comparationWorld_two}" NO es Anagrama de "${comparationWorld_one}"</p>`        
     }
+    
 }
